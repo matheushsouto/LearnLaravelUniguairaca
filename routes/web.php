46135634cf827762412368ac2699983a1fb1b73c
Matheus Souto <frameworktecnologia37@localiza.com>
2024-04-30 17:45:01 -0300

    ✨feature: Adicionando autenticação e middleware

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,38 @@
 <?php
 
 use App\Http\Controllers\ContactController;
-use App\Models\Flight;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contato', [ContactController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::post('/contato/store', [ContactController::class, 'store'])->name('contato.store');
+Route::middleware('auth')->group(function () {
+    Route::get('/contato', [ContactController::class, 'index']);
+    Route::post('/contato/store', [ContactController::class, 'store'])->name('contato.store');
+    Route::put('/contato/update', [ContactController::class, 'update'])->name('contato.update');
+    Route::delete('/contato/delete/{id}', [ContactController::class, 'destroy'])
+        ->name('contato.destroy');
+    Route::get('/contato/decripty', [ContactController::class, 'decripty']);
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::put('/contato/update', [ContactController::class, 'update'])->name('contato.update');
-
-Route::delete('/contato/delete/{id}', [ContactController::class, 'destroy'])
-->name('contato.destroy');
-
-Route::get('/contato/decripty', [ContactController::class, 'decripty']);
-
-
-
+require __DIR__ . '/auth.php';
