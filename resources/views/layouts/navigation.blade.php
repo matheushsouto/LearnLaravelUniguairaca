44226b4d35fdad2980a80345f6b81e03903ee6c2
Matheus Souto <frameworktecnologia37@localiza.com>
2024-05-21 14:55:38 -0300

    ✨feature: Adicionando as views do projeto

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,8 +12,11 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link href="{{ url('/dashboard') }}">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                    <x-nav-link href="{{ url('/alunos') }}">
+                        {{ __('Alunos') }}
                     </x-nav-link>
                 </div>
             </div>
